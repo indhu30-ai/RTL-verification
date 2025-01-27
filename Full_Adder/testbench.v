592d983a0fd1030bfa72a52a7215f167566b49a6
indhu30-ai <indhukn3@gmail.com>
2025-01-27 15:42:11 +0530

    Create testbench.v

---
+++ Full_Adder/testbench.v
@@ -0,0 +1,20 @@
+module fa_tb;
+reg a,b,cin;
+wire sum,cout;
+full_adder DUT (a,b,cin,sum,cout);
+
+
+initial begin
+#10 a = 0; b = 0; cin = 0;
+#10 a = 0; b = 0; cin = 1;
+#10 a = 0; b = 1; cin = 0;
+#10 a = 0; b = 1; cin = 1;
+#10 a = 1; b = 0; cin = 0;
+#10 a = 1; b = 0; cin = 1;
+#10 a = 1; b = 1; cin = 0;
+#10 a = 1; b = 1; cin = 1;
+
+
+end
+
+endmodule
